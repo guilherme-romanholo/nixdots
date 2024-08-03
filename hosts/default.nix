919a9910c096f615184da9407695c4d780b6d70e
guilherme-romanholo <guilhermeromanholobofo@gmail.com>
2024-08-02 21:32:42 -0300

    feat: add volt host

--- hosts/default.nix
+++ hosts/default.nix
@@ -23,5 +23,27 @@
         }
       ];
     }
+    {
+      # Host
+      hostname = "volt";
+      profile = "personal";
+      bootmode = "uefi";
+      grubDevice = "null";
+      systemVersion = "24.05";
+      videoDriver = "amdgpu";
+      # Locale
+      locale = "pt_BR.UTF-8";
+      timezone = "America/Sao_Paulo";
+      kblayout = "br";
+      # Users
+      users = [
+        {
+          username = "guilherme";
+          email = "guilhermeromanholobofo@gmail.com";
+          gituser = "guilherme-romanholo";
+          groups = ["wheel" "networkmanager"];
+        }
+      ];
+    }
   ];
 }
