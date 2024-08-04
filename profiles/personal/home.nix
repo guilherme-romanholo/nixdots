c323903ea0c8ced6a6cdaa848add6cf939015819
guilherme-romanholo <guilhermeromanholobofo@gmail.com>
2024-08-04 11:51:03 -0300

    feat: enable desktop home configs in personal profile

--- profiles/personal/home.nix
+++ profiles/personal/home.nix
@@ -1,5 +1,18 @@
 {
+  outputs,
+  ...
+}: {
   imports = [
     ../general/home.nix
+    outputs.homeManagerModules.desktop
   ];
+
+  desktop = {
+    # Enable home desktop configurations
+    enable = true;
+    # Enable bspwm home config
+    bspwm.enable = true;
+    # Enable desktop home apps
+    apps.enable = true;
+  };
 }
