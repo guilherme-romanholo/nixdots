ff00664d22d7a6c8a0dd40b73c6270c670f5eff2
guilherme-romanholo <guilhermeromanholobofo@gmail.com>
2024-08-04 14:12:50 -0300

    feat: add bspwm windows binds

--- modules/home-manager/desktop/bspwm/sxhkd.nix
+++ modules/home-manager/desktop/bspwm/sxhkd.nix
@@ -14,17 +14,19 @@ in {
     services.sxhkd = {
       enable = true;
       keybindings = {
-	### Applications ###
-
-	# Terminal 
+	# Applications
 	"super + Return" = "${pkgs.kitty}/bin/kitty";
-
-	### Bspwm Hotkeys ###
-
-	# Quit and restart bspwm
+	# Bspwm Hotkeys
 	"super + ctrl + {q,r}" = "${pkgs.bspwm}/bin/bspc {quit,wm -r}";
-	# Close and kill
 	"super + {_,shift + }q" = "${pkgs.bspwm}/bin/bspc node -{c,k}";
+	# State
+	"super + {t,shift + t,s,f}" = "${pkgs.bspwm}/bin/bspc node -t {tiled,pseudo_tiled,floating,fullscreen}";
+	# Focus
+	"super + shift + {h,j,k,l}" = "${pkgs.bspwm}/bin/bspc node -f {west,south,north,east}";
+	# Move/resize
+	"super + ctrl + {h,j,k,l}" = "${pkgs.bspwm}/bin/bspc node -v {-20 0,0 20,0 -20,20 0}";
+	"super + alt + {h,j,k,l}" = "${pkgs.bspwm}/bin/bspc node -z {left -20 0,bottom 0 20,top 0 -20,right 20 0}";
+	"super + alt + shift + {h,j,k,l}" = "${pkgs.bspwm}/bin/bspc node -z {right -20 0,top 0 20,bottom 0 -20,left 20 0}";
       };
     };
   };
