79f836bff0a59f09ddb1e57a2ed90b4bbff9a26e
guilherme <guilhermeromanholobofo@gmail.com>
2024-08-04 23:04:27 -0300

    chore: add polybar style

--- modules/home-manager/desktop/bspwm/polybar.nix
+++ modules/home-manager/desktop/bspwm/polybar.nix
@@ -18,25 +18,70 @@ in {
         "bar/top" = {
           width = "100%";
           height = "3%";
+
           radius = 0;
-          modules-left = "bspwm";
+
+	  bottom = false;
+	  fixed-center = true;
+
+	  background = "${config.lib.stylix.colors.base00}";
+	  foreground = "${config.lib.stylix.colors.base05}";
+
+	  format-background = "${config.lib.stylix.colors.base00}";
+	  format-foreground = "${config.lib.stylix.colors.base05}";
+
+	  line-size = 2;
+	  line-color = "${config.lib.stylix.colors.base05}";
+
+	  border-size = 4;
+	  border-color = "${config.lib.stylix.colors.base00}";
+
+	  font-0 = "${config.stylix.fonts.monospace.name}:size=${toString config.stylix.fonts.sizes.applications};3";
+
+	  padding = 1;
+
+          modules-left = "mod sep bspwm";
           modules-center = "date";
+        };
+
+        "module/mod" = {
+          type = "internal/bspwm";
+	  format = "<label-mode>";
+
+	  format-background = "${config.lib.stylix.colors.base0A}";
+	  format-foreground = "${config.lib.stylix.colors.base00}";
+	  format-padding = 1;
+	  
+	  label-monocle = "Monocle";
+	  label-tiled = "Tiled";
+	  label-floating = ", Float";
+	  label-pseudotiled = ", Pseudo";
+	  label-fullscreen = ", Full";
         };
 
         "module/date" = {
           type = "internal/date";
           internal = 5;
-          date = "%d.%m.%y";
+          date = "%d/%m/%y";
           time = "%H:%M";
-          label = "%time% %date%";
+          label = "%time% | %date%";
         };
 
         "module/bspwm" = {
           type = "internal/bspwm";
+	  format = "<label-state>";
+	  label-padding = 2;
           pin-workspaces = true;
-          ws-icon-default = "o";
-          label-focused-foreground = "#aaaaaa";
         };
+
+	"module/sep"= {
+	  type = "custom/text";
+	  content = " ";
+	  
+	  content-background = "${config.lib.stylix.colors.base00}";
+	  content-foreground = "${config.lib.stylix.colors.base01}";
+	  content-padding = 2;
+	};
       };
     };
   };
