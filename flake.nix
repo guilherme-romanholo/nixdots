115d0fad3953dc75f66654edb015a5f7676d0f77
guilherme <guilhermeromanholobofo@gmail.com>
2024-09-13 18:11:53 -0300

    feat: add home-manager input

--- flake.nix
+++ flake.nix
@@ -6,6 +6,11 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     # Nixpkgs Unstable
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    # Home manager
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     # Disko
     disko = {
       url = "github:nix-community/disko";
@@ -16,6 +21,7 @@
   outputs = {
     self,
     nixpkgs,
+    home-manager,
     ...
   } @ inputs: let
     inherit (self) outputs;
@@ -28,7 +34,7 @@
           (builtins.removeAttrs (builtins.readDir ./hosts) ["template"])));
 
     # Define lib to use in flake
-    lib = nixpkgs.lib;
+    lib = nixpkgs.lib // home-manager.lib;
 
     # Supported systems for your flake packages, shell, etc.
     systems = [
