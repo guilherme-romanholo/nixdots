5352cec3d62ea2fed18eea13f77023671f9b4350
guilherme-romanholo <guilhermeromanholobofo@gmail.com>
2024-09-14 20:25:47 +0000

    feat

---
+++ hosts/vivobook/configuration.nix
@@ -0,0 +1,65 @@
+{
+  inputs,
+  outputs,
+  pkgs,
+  ...
+}: let
+  vars = import ./default.nix;
+in  {
+  imports = [
+    # Modules
+    outputs.nixosModules.core
+    outputs.nixosModules.desktop
+    outputs.nixosModules.optional
+
+    # Hardware
+    ./hardware-configuration.nix
+  ];  
+
+  environment.systemPackages = with pkgs; [
+    neovim
+  ];
+
+  ############
+  # Optional #
+  ############
+
+  optional = {
+    wsl = {
+      enable = true;
+      defaultUser = builtins.head vars.users;
+    };
+  };
+
+  ########
+  # Core #
+  ########
+
+  core = {
+    # Config timezone
+    localtime = {
+      enable = true;
+      locale = "pt_BR.UTF-8";
+      consoleKeymap = "br-abnt2";
+      timezone = "America/Sao_Paulo";
+    };
+    # Config nix
+    nix = {
+      enable = true;
+      substituters = ["https://devenv.cachix.org"];
+      trustedKeys = ["devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="];
+    };
+    # Config network
+    network = {
+      enable = true;
+      hostname = vars.hostname;
+    };
+    # Config Users
+    user = {
+      enable = true;
+      users = vars.users;
+    };
+  };
+
+  system.stateVersion = vars.stateVersion;
+}
---
+++ hosts/vivobook/default.nix
@@ -0,0 +1,22 @@
+{
+  # System informations
+  hostname = "vivobook";
+  system = "x86_64-linux";
+  stateVersion = "24.05";
+
+  # Users informations
+  users = [
+    {
+      # User
+      username = "guilherme";
+      groups = ["networkmanager" "wheel"];
+      # Git
+      gituser = "guilherme-romanholo";
+      email = "guilhermeromanholobofo@gmail.com";
+      # Suites
+      suites = ["terminal" "apps"];
+      # Only if desktop suite is enabled
+      desktops = [];
+    }
+  ];
+}
