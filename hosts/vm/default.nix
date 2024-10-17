479916936c2d7865092221feb1515e9251def578
guilherme <guilhermeromanholobofo@gmail.com>
2024-10-17 07:10:06 -0300

    chore: update config structure

--- hosts/vm/default.nix
+++ hosts/vm/default.nix
@@ -1,17 +1,19 @@
 {
-  imports = [
-    ./hardware-configuration.nix
+  hostname = "vm";
+  system = "x86_64-linux";
+  stateVersion = "24.05";
+
+  locale = "pt_BR.UTF-8";
+  timezone = "America/Sao_Paulo";
+
+  grubDevice = "/dev/vda";
+
+  users = [
+    {
+      username = "nixos";
+      groups = ["networkmanager" "wheel"];
+    }
   ];
 
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  modules = {
-    # Optional Modules
-    graphical.enable = true;
-    openssh.enable = true;
-    printer.enable = true;
-    xserver.layout = "br";
-  };
+  config = import ./configuration.nix;
 }
