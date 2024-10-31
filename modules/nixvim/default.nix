0dc7820fecf8fcd577d9a563281973d728d42514
guilherme <guilhermeromanholobofo@gmail.com>
2024-10-31 13:31:39 -0300

    feat: add nixvim plugins

--- modules/nixvim/default.nix
+++ modules/nixvim/default.nix
@@ -1,4 +1,44 @@
 {
-  # Import all your configuration modules here
-  imports = [./bufferline.nix];
+  imports = [
+    ./settings.nix
+    ./keys.nix
+
+    # Mini
+    ./plugins/mini
+
+    # Lsp
+    ./plugins/lsp/lsp.nix
+    ./plugins/lsp/conform.nix
+    ./plugins/lsp/lspsaga.nix
+
+    # Git
+    ./plugins/git/gitsigns.nix
+
+    # Tree
+    ./plugins/tree/neotree.nix
+    ./plugins/tree/treesitter.nix
+
+    # Completion
+    ./plugins/completion/cmp.nix
+    ./plugins/completion/lspkind.nix
+
+    # Snippets
+    ./plugins/snippets/luasnip.nix
+
+    # Utils
+    ./plugins/utils/harpoon.nix
+    ./plugins/utils/whichkey.nix
+    ./plugins/utils/floaterm.nix
+    ./plugins/utils/autopairs.nix
+    ./plugins/utils/guess-ident.nix
+    ./plugins/utils/todo-comments.nix
+
+    # UI
+    ./plugins/ui/precognition.nix
+    ./plugins/ui/bufferline.nix
+    ./plugins/ui/toggleterm.nix
+    ./plugins/ui/lualine.nix
+    ./plugins/ui/noice.nix
+    ./plugins/ui/alpha.nix
+  ];
 }
---
+++ modules/nixvim/settings.nix
@@ -0,0 +1,25 @@
+{
+  colorschemes.base16 = {
+    enable = true;
+    setUpBar = false;
+    colorscheme = import ./colors/gruvbox.nix {};
+  };
+
+  # clipboard.providers.wl-copy.enable = true;
+
+  opts = {
+    # Line numbers
+    number = true;
+    relativenumber = true;
+
+    # Signcolumn
+    signcolumn = "yes";
+
+    # Tabs
+    smartindent = true;
+    shiftwidth = 2;
+
+    # Text wrap
+    wrap = true;
+  };
+}
